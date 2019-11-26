b19f9a1285ba7c4c1925a3888b7bf2ca36e713fc
Mario Nieto <mnieto156@alumno.uned.es>
2019-11-26 19:25:02 +0100

    Tests JUnit de repositorio de Alumnos

--- src/test/groovy/es/uned/lsi/PL_ci/repository/AlumnoRepositoryIntegrationTest.groovy
+++ src/test/groovy/es/uned/lsi/PL_ci/repository/AlumnoRepositoryIntegrationTest.groovy
@@ -1,6 +1,7 @@
 package es.uned.lsi.PL_ci.repository
 
 import es.uned.lsi.PL_ci.entity.Alumno
+import es.uned.lsi.PL_ci.entity.User
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,8 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.junit4.SpringRunner
 
+import static org.assertj.core.api.Assertions.assertThat
 import static org.junit.Assert.assertNotNull
-import static org.junit.Assert.assertTrue
 
 @RunWith(SpringRunner)
 @DataJpaTest
@@ -35,25 +36,73 @@ class AlumnoRepositoryIntegrationTest {
         def foundAlumn = alumnoRepository.findByAlumnoId(testAlumn.alumnoId)
 
         assertNotNull foundAlumn
-        assertTrue foundAlumn.nombre == testAlumn.nombre
+        assertThat(foundAlumn).isEqualTo testAlumn
     }
 
     @Test
-    void findAll() {
+    void whenFindAll_thenReturnAlumnos() {
+
+        alumnoRepository.save new Alumno(
+                nombre: "Nombre",
+                apellido1: "Ap1",
+                correo: "nomAp1@test.com",
+                curso: "2019-2020-PL2")
+        assertThat(alumnoRepository.findAll()).hasSize 1
+
+        alumnoRepository.save new Alumno(
+                nombre: "Nombre",
+                apellido1: "Ap2",
+                correo: "nomAp2@test.com",
+                curso: "2019-2020-PL2"
+        )
+        assertThat(alumnoRepository.findAll()).hasSize 2
+    }
+
+    @Test
+    void whenFindByCurso_thenReturnAlumnos() {
+        alumnoRepository.save new Alumno(
+                nombre: "Nombre",
+                apellido1: "Ap1",
+                correo: "nomAp1@test.com",
+                curso: "2019-2020-PL2")
+
+        alumnoRepository.save new Alumno(
+                nombre: "Nombre",
+                apellido1: "Ap2",
+                correo: "nomAp2@test.com",
+                curso: "2019-2020-PL2"
+        )
+        alumnoRepository.save new Alumno(
+                nombre: "Nombre",
+                apellido1: "Ap2",
+                correo: "nomAp2@test.com",
+                curso: "2019-2020-PL1"
+        )
+        assertThat(alumnoRepository.findAll()).hasSize 3
+        assertThat(alumnoRepository.findByCurso("2019-2020-PL2")).hasSize 2
+    }
+
+    @Test
+    void whenFindByUsername_thenReturnAlumno() {
         Alumno testAlumn
         testAlumn = new Alumno(
                 nombre: "Nombre",
                 apellido1: "Ap1",
                 correo: "nomAp1@test.com",
                 curso: "2019-2020-PL2")
+        testAlumn.user = new User(
+                username: "nomAp1",
+                password: "password"
+        )
+        testAlumn.user.alumno = testAlumn
         alumnoRepository.save testAlumn
-        def foundAlumns = alumnoRepository.findAll()
-        assertTrue foundAlumns.size() == 1
-    }
 
+        assertThat(alumnoRepository.findByUserUsername(testAlumn.user.username)).isEqualTo testAlumn
+    }
 
     @After
     void cleanTestDB() {
-
+        //alumnoRepository.deleteAll()
+        //assertThat(alumnoRepository.findAll()).hasSize 0
     }
 }
